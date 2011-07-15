8b69f969895eef63280404ca5c0bddca65de419d
oyiptong <oyiptong@zompf.com>
2011-07-14 23:49:36 -0700

    stackrocket.com instead of koi.io in memcache message

--- welcome/views.py
+++ welcome/views.py
@@ -4,7 +4,7 @@ from django.core.cache import cache
 def index(request):
     cache.set('secret', 'roses are red, violets are blue')
     extra_context = {
-            'motd' : 'Welcome to the koi.io demo',
+            'motd' : 'Welcome to the stackrocket.com demo',
             'secret' : cache.get('secret')
     }
     return direct_to_template(request, template="index.html", extra_context=extra_context)
